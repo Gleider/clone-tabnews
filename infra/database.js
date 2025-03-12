4e4873f441f719edebc7fbdf1f0daeab71e526e0
Gleider <gleider.campos@twism.com>
2025-03-12 20:39:07 -0300

    fix `eslint` linting

--- infra/database.js
+++ infra/database.js
@@ -34,7 +34,9 @@ function getSSLValues() {
   }
   return process.env.NODE_ENV === "production" ? true : false;
 }
-export default {
+
+const database = {
   query,
   getNewClient,
 };
+export default database;
