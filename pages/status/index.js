3376488218d9e56c59511343be832af1405ee670
Gleider <gleider.campos@twism.com>
2025-05-21 21:01:58 -0300

    feat: add initial version of status page

---
+++ pages/status/index.js
@@ -0,0 +1,49 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+  console.log("data");
+  console.log(data);
+  let databaseStatusInformation = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <div>
+          Última atualização:{" "}
+          {new Date(data.updated_at).toLocaleString("pt-BR")}
+        </div>
+        <div>Versão: {data.dependencies.database.version}</div>
+        <div>
+          Conexões abertas: {data.dependencies.database.opened_connections}
+        </div>
+        <div>
+          Conexões máximas: {data.dependencies.database.max_connections}
+        </div>
+      </>
+    );
+    return (
+      <div>
+        <h2>Banco de dados</h2>
+        {databaseStatusInformation}
+      </div>
+    );
+  }
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+    </>
+  );
+}
